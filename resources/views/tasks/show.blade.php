4235d7d63fee5efc6b42df0c1dd61db1eb44856a
rena.tanaka <rena.tanaka@rakuten.com>
2018-05-30 06:54:10 +0000

    kadaii

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -5,7 +5,7 @@
 <h1>id = {{ $task->id }} のタスク詳細ページ</h1>
 
     <p>ステータス: {{ $task->content }}</p>
-    <p>タスク: {{ $task->title }}</p>
+    <p>タスク: {{ $task->status }}</p>
     
      {!! link_to_route('tasks.edit', 'このタスク編集', ['id' => $task->id]) !!}
 
