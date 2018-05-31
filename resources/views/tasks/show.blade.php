265ca48181b1e9ef809345a16a177f3f63cc942b
rena.tanaka <rena.tanaka@rakuten.com>
2018-05-31 03:31:20 +0000

    gridkadai

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -4,14 +4,26 @@
 
 <h1>id = {{ $task->id }} のタスク詳細ページ</h1>
     
-    <p>タスク: {{ $task->status }}</p>
-    <p>ステータス: {{ $task->content }}</p>
+    <table class="table table-bordered">
+        <tr>
+            <th>id</th>
+            <td>{{ $task->id }}</td>
+        </tr>
+        <tr>
+            <th>ステータス</th>
+            <td>{{ $task->status }}</td>
+        </tr>
+        <tr>
+            <th>タスク</th>
+            <td>{{ $task->content }}</td>
+        </tr>
+    </table>
     
     
-     {!! link_to_route('tasks.edit', 'このタスク編集', ['id' => $task->id]) !!}
+     {!! link_to_route('tasks.edit', 'このタスクを編集', ['id' => $task->id], ['class' => 'btn btn-default']) !!}
 
-    {!! Form::model($task, ['route' => ['tasks.destroy', $task->id], 'method' => 'delete']) !!}
-        {!! Form::submit('削除') !!}
+   {!! Form::model($task, ['route' => ['tasks.destroy', $task->id], 'method' => 'delete']) !!}
+        {!! Form::submit('削除', ['class' => 'btn btn-danger']) !!}
     {!! Form::close() !!}
 
 @endsection
